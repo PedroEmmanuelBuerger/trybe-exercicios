10bb54be4bec19faea9c5c1ba811caae2ba371ee
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-09-21 15:03:08 -0300

    exercicios javascript

---
+++ modulo_1 - fundamentos/bloco_04-introducao-a-javascript-e-logica-de-programacao/01-primeiros-passos/exercicio1.js
@@ -0,0 +1,8 @@
+const myName = 'Pedro Emmanuel';
+const birthCity = 'blumenau';
+let birthYear = 2001;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = 'florianopolis';
+console.log(birthCity);
