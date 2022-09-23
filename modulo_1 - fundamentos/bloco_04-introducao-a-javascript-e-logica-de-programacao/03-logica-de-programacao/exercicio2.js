2b18af0aab55eaf96327a1d78901d17439f1b35b
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-09-23 16:54:21 -0300

    começando exercicios lógica

--- modulo_1 - fundamentos/bloco_04-introducao-a-javascript-e-logica-de-programacao/03-logica-de-programacao/exercicio2.js
+++ modulo_1 - fundamentos/bloco_04-introducao-a-javascript-e-logica-de-programacao/03-logica-de-programacao/exercicio2.js
@@ -1,7 +1,7 @@
 let aocontrario = ''
 let word = 'tryber';
 
-for (let index = word.length; index <= 0; index --){
+for (let index = word.length -1; index >= 0; index -=1){
 aocontrario += word[index]
 }
 console.log(aocontrario)
