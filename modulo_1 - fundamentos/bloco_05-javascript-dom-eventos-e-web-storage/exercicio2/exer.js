2a06c97b843b49a4c428adc06ed8f46c09570037
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-09-28 17:57:52 -0300

    resolvendo exercicios DOM

---
+++ modulo_1 - fundamentos/bloco_05-javascript-dom-eventos-e-web-storage/exercicio2/exer.js
@@ -0,0 +1,12 @@
+document.getElementsByTagName('p')[1].innerText = ('integrado na area de programação. bem remunerado e mais maduro');
+let quadrado = document.getElementsByClassName('main-content')
+quadrado[0].style.backgroundColor = ('rgb(76,164,109')
+let vermelho = document.getElementsByClassName('center-content')
+vermelho[0].style.backgroundColor = ('white')
+cabecalho = document.querySelector('h1')
+cabecalho.innerText = ('Exercício - JavaScript')
+paragrafo = document.querySelector('p').style.textTransform = "uppercase";
+escrever = document.querySelectorAll('p')
+for (let index = 0; index <= escrever.length - 1; index = index + 1) {
+    console.log(escrever[index].innerText)
+}
