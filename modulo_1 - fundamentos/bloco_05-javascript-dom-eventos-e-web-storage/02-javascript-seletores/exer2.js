95afaa00a3d57d2a97670947eac22a76ba1134df
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-09-29 16:33:43 -0300

    resolvendo exercicios DOM

---
+++ modulo_1 - fundamentos/bloco_05-javascript-dom-eventos-e-web-storage/02-javascript-seletores/exer2.js
@@ -0,0 +1,12 @@
+let um = document.getElementById('elementoOndeVoceEsta')
+let dois = um.parentNode
+dois.style.backgroundColor = 'blue'
+dois.style.color = 'white'
+seis = dois.innerText
+let tres = document.getElementById('primeiroFilhoDoFilho').innerText = 'pedro'
+let quatro = document.getElementById('pai').firstElementChild
+let cinco = um.firstElementChild
+let sete = dois.lastElementChild.previousElementSibling
+let oito = um.nextElementSibling
+console.log(oito)
+
