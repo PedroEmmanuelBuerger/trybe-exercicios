bb2f669958146450d994f57557ca010fd83c1ed4
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-10-19 16:47:48 -0300

    exercicios

---
+++ modulo_1 - fundamentos/bloco_07-introducao-a-javascript-es6-e-teste-unitarios/03-jest/sum.test.js
@@ -0,0 +1,6 @@
+// sum.js
+const sum = (a, b) => a + b;
+
+test('soma dois valores', () => {
+  expect(sum(2, 3)).toEqual(5);
+});
---
+++ modulo_1 - fundamentos/bloco_07-introducao-a-javascript-es6-e-teste-unitarios/03-jest/teste.test.js
@@ -0,0 +1,9 @@
+describe('Teste quando uma variável é declarada como: ', () => {
+    const zero = 0;
+    const um = 1;
+  }
+  
+)  test('expect(zero).toBe(null);', () => {
+    expect(zero).toBe(null);
+    expect(um).not.toBe(null);
+  });
